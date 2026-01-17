04e0df52a8f26cfd99d63263f7510612a8fbfa1c
BlockShane <72456103+2shanez@users.noreply.github.com>
2026-01-17 01:39:14 +0000

    Show only years in work experience dates

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -21,20 +21,16 @@ export function readingTime(html: string) {
 }
 
 export function dateRange(startDate: Date, endDate?: Date | string): string {
-  const startMonth = startDate.toLocaleString("default", { month: "short" });
   const startYear = startDate.getFullYear().toString();
-  let endMonth;
   let endYear;
 
   if (endDate) {
     if (typeof endDate === "string") {
-      endMonth = "";
       endYear = endDate;
     } else {
-      endMonth = endDate.toLocaleString("default", { month: "short" });
       endYear = endDate.getFullYear().toString();
     }
   }
 
-  return `${startMonth}${startYear} - ${endMonth}${endYear}`;
+  return `${startYear} - ${endYear}`;
 }
